f7516632229a870971004c6ce0d4619eef353ebb
Diego Lemos <diegolemosderesende@gmail.com>
2016-07-17 17:45:48 +0200

    Preparing EventHandler towards OCP.

--- src/main/scala/EventHandler.scala
+++ src/main/scala/EventHandler.scala
@@ -1,27 +1,50 @@
 class EventHandler(messages: Messages, users: Users, display: Display) {
 
+  val postMessageObserver = new PostMessageObserver(messages, users)
+  val addFollowerObserver = new AddFollowerObserver(users)
+  val displayWallObserver = new DisplayWallObserver(users, messages, display)
+  val readMessagesObserver = new ReadMessagesObserver(users, messages, display)
+
   def handle(event: Event): Unit = {
     event match {
       case post: PostMessage =>
-        val maybeAUser = users.findByName(post.userName)
-        val user = maybeAUser.getOrElse(createUser(post.userName))
-        messages.add(Message(user, post.message, post.postTime))
+        postMessageObserver.notify(post)
 
       case follow: AddFollower =>
-        val user = findUserBy(follow.user)
-        val anotherUser = findUserBy(follow.followee)
-        users.addFollower(user, anotherUser)
+        addFollowerObserver.notify(follow)
 
       case wall: DisplayWall =>
-        val user = findUserBy(wall.user)
-        val userWall = messages.findBy((users followedBy user) + user)
-        display.wall(userWall)
+        displayWallObserver.notify(wall)
 
       case read: ReadMessages =>
-        val user = findUserBy(read.user)
-        val userMessages = messages.findBy(user)
-        display.timeline(userMessages)
+        readMessagesObserver.notify(read)
     }
+  }
+}
+
+sealed trait Observer[Event] {
+  def notify(event: Event)
+}
+
+class PostMessageObserver(messages: Messages, users: Users) extends Observer[PostMessage] {
+  override def notify(post: PostMessage) = {
+    val maybeAUser = users.findByName(post.userName)
+    val user = maybeAUser.getOrElse(createUser(post.userName))
+    messages.add(Message(user, post.message, post.postTime))
+  }
+
+  private def createUser(userName: String): User = {
+    val user = User(userName.trim)
+    users.add(user)
+    user
+  }
+}
+
+class AddFollowerObserver(users: Users) extends Observer[AddFollower] {
+  override def notify(addFollower: AddFollower) = {
+    val user = findUserBy(addFollower.user)
+    val anotherUser = findUserBy(addFollower.followee)
+    users.addFollower(user, anotherUser)
   }
 
   private def findUserBy(userName: String): User = {
@@ -32,10 +55,39 @@ class EventHandler(messages: Messages, users: Users, display: Display) {
         throw new RuntimeException("Oups, the user \"" + userName + "\" doesn't exist.")
     }
   }
+}
 
-  private def createUser(userName: String): User = {
-    val user = User(userName.trim)
-    users.add(user)
-    user
+class DisplayWallObserver(users: Users, messages: Messages, display: Display) extends Observer[DisplayWall] {
+  override def notify(displayWall: DisplayWall) = {
+    val user = findUserBy(displayWall.user)
+    val userWall = messages.findBy((users followedBy user) + user)
+    display.wall(userWall)
+  }
+
+  private def findUserBy(userName: String): User = {
+    users.findByName(userName) match {
+      case Some(user) =>
+        user
+      case None =>
+        throw new RuntimeException("Oups, the user \"" + userName + "\" doesn't exist.")
+    }
+  }
+}
+
+class ReadMessagesObserver(users: Users, messages: Messages, display: Display) extends Observer[ReadMessages] {
+  override def notify(readMessages: ReadMessages) = {
+    val user = findUserBy(readMessages.user)
+    val userMessages = messages.findBy(user)
+    display.timeline(userMessages)
+  }
+
+
+  private def findUserBy(userName: String): User = {
+    users.findByName(userName) match {
+      case Some(user) =>
+        user
+      case None =>
+        throw new RuntimeException("Oups, the user \"" + userName + "\" doesn't exist.")
+    }
   }
 }
